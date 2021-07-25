17e981ae153f822cc0c3effae201e65944bb4b40
JOHN NASIMBA <nasimba4john@gmail.com>
2021-07-25 17:00:16 +0200

    Building the UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import ImageSlider from './components/ImageSlider';
+import { SliderData } from './components/sliderData';
 
 function App() {
   return (
     <div className="App">
-      <ImageSlider />
+      <ImageSlider slides={SliderData} />
     </div>
   );
 }
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SliderData } from './sliderData';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
 
-function ImageSlider() {
+function ImageSlider({slides}) {
+    const [current, setCurrent] = useState(0);
+    const length = slides.length
+
+//    Return nothing when slides array contains no data
+
+    if(!Array.isArray(slides) ||slides.length <= 0) {
+        // Array.isArray() function is used to check wether an object is an array
+        return null
+    }
+
     return (
-        <div>
+        <div className='image-slider'>
+            <FaArrowAltCircleLeft className="left-arrow"  />
+            <FaArrowAltCircleRight className="right-arrow" />
             {
                 SliderData.map((slide, index) => {
-                    return <img key={index} src={process.env.PUBLIC_URL + slide.image} alt="My images" />
+                    return <img key={index} src={process.env.PUBLIC_URL + slide.image} className="image" alt="My images" />
                 })
             }
         </div>
